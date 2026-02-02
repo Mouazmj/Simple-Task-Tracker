31cf51d823d96b69f19299b6dbb9d6e52733148b
Mouaz Jamal <mj225ih@student.lnu.se>
2026-02-02 11:57:31 +0100

    Set up the routes and and use middleware logic

--- index.js
+++ index.js
@@ -2,8 +2,24 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 
+app.use(express.urlencoded({ extended: true }))
+
 const tasks = [
     { id: 1, title: 'Träna', priority: 'Hög', completed: false },
     { id: 2, title: 'Äta', priority: 'Medel', completed: true },
     { id: 3, title: 'Diska', priority: 'Låg', completed: false }
 ]
+
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+
+})
+
+app.get('/add', (req, res) => {
+
+})
+
+app.post('/add', (req, res) => {
+
+})
